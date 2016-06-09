7ce93c61fc9de040f46f3d53878ffee53cf1c106
Joseph A. Yasi <joe.yasi@gmail.com>
2016-06-08 22:47:41 -0400

    Build with newer versions of gcc.

--- gccvars.mk
+++ gccvars.mk
@@ -1,8 +1,12 @@
 CC=gcc
 CXX=g++
 DEBUGFLAG=-g
-CFLAGS=-O2 -march=core2
+CFLAGS=-O3 -ftree-vectorize -march=native -flto -floop-parallelize-all -fgraphite-identity -floop-block -floop-strip-mine -floop-interchange
+#CFLAGS=-O0 -g -DDEBUG
 CXXFLAGS=$(CFLAGS)
+CXXPROGFLAGS=-fuse-linker-plugin -fwhole-program
+#CXXPROGFLAGS=
 INCLUDES=-I/sw/include -I../matrixlib
 LIBS=-L/sw/lib -lgsl -lgslcblas
-AR=ar
+AR=gcc-ar
+RANLIB=gcc-ranlib
